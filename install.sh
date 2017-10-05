28483ce235fb5ca8365a9b9bb6692ff433c94440
jamesarems <jamesarems@gmail.com>
2017-10-05 15:22:36 +0400

    Update

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ read -p "License server IP:" a
 LOC=`pwd`
 mkdir /var/license
 mkdir /var/license/file
-cp $LOC/update.sh /var/license/
 sed -i "s/serverip/$a/g" $LOC/update.sh
+cp $LOC/update.sh /var/license/
 chmod +x /var/license/update.sh
 echo "sh /var/license/update.sh" >> /etc/rc.local
 cp $LOC/server /usr/bin/server
